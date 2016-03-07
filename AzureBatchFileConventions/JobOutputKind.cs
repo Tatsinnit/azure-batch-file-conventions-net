1f5ced11b82229df8568544a2ae86d3eaab4c2bc
Ivan Towlson <ivan.towlson@microsoft.com>
2016-03-07 16:51:11 +1300

    Output kinds for task and job output persistence

---
+++ AzureBatchFileConventions/JobOutputKind.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Batch.Conventions.Files
+{
+    public sealed class JobOutputKind : IEquatable<JobOutputKind>
+    {
+        public static readonly JobOutputKind JobOutput = new JobOutputKind("JobOutput");
+        public static readonly JobOutputKind JobPreview = new JobOutputKind("JobPreview");
+
+        private static readonly StringComparer TextComparer = StringComparer.Ordinal;  // case sensitive, since we preserve case when stringising
+
+        private readonly string _text;
+
+        private JobOutputKind(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+            if (String.IsNullOrEmpty(text))
+            {
+                throw new ArgumentException("Text must not be empty", nameof(text));
+            }
+
+            _text = text;
+        }
+
+        public static JobOutputKind Custom(string text)
+        {
+            return new JobOutputKind(text);
+        }
+
+        public override string ToString()
+        {
+            return _text;
+        }
+
+        public bool Equals(JobOutputKind other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            return TextComparer.Equals(other._text, _text);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as JobOutputKind);
+        }
+
+        public override int GetHashCode()
+        {
+            Debug.Assert(_text != null);
+
+            return TextComparer.GetHashCode(_text);
+        }
+
+        public static bool operator ==(JobOutputKind x, JobOutputKind y)
+        {
+            if (ReferenceEquals(x, null))
+            {
+                return ReferenceEquals(y, null);
+            }
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(JobOutputKind x, JobOutputKind y)
+        {
+            return !(x == y);
+        }
+    }
+}
---
+++ AzureBatchFileConventions/TaskOutputKind.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Batch.Conventions.Files
+{
+    public sealed class TaskOutputKind : IEquatable<TaskOutputKind>
+    {
+        public static readonly TaskOutputKind TaskOutput = new TaskOutputKind("TaskOutput");
+        public static readonly TaskOutputKind TaskPreview = new TaskOutputKind("TaskPreview");
+        public static readonly TaskOutputKind TaskLog = new TaskOutputKind("TaskLog");
+        public static readonly TaskOutputKind TaskIntermediate = new TaskOutputKind("TaskIntermediate");
+
+        private static readonly StringComparer TextComparer = StringComparer.Ordinal;  // case sensitive, since we preserve case when stringising
+
+        private readonly string _text;
+
+        private TaskOutputKind(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+            if (String.IsNullOrEmpty(text))
+            {
+                throw new ArgumentException("Text must not be empty", nameof(text));
+            }
+
+            _text = text;
+        }
+
+        public static TaskOutputKind Custom(string text)
+        {
+            return new TaskOutputKind(text);
+        }
+
+        public override string ToString()
+        {
+            return _text;
+        }
+
+        public bool Equals(TaskOutputKind other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            return TextComparer.Equals(other._text, _text);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TaskOutputKind);
+        }
+
+        public override int GetHashCode()
+        {
+            Debug.Assert(_text != null);
+
+            return TextComparer.GetHashCode(_text);
+        }
+
+        public static bool operator ==(TaskOutputKind x, TaskOutputKind y)
+        {
+            if (ReferenceEquals(x, null))
+            {
+                return ReferenceEquals(y, null);
+            }
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(TaskOutputKind x, TaskOutputKind y)
+        {
+            return !(x == y);
+        }
+    }
+}
