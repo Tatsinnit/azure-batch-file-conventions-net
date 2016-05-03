f8668d0c51e6641701426eac2db728b90a8798b4
Ivan Towlson <ivan.towlson@microsoft.com>
2016-05-04 10:39:47 +1200

    Added utility class for creating valid blob storage container names for jobs

---
+++ AzureBatchFileConventions.UnitTests/Utilities/ContainerNameUtilsTests.cs
@@ -0,0 +1,81 @@
+﻿using FsCheck;
+using FsCheck.Xunit;
+using Microsoft.Azure.Batch.Conventions.Files.UnitTests.Generators;
+using Microsoft.Azure.Batch.Conventions.Files.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Azure.Batch.Conventions.Files.UnitTests.Utilities
+{
+    [Arbitrary(typeof(BatchIdGenerator))]
+    public class ContainerNameUtilsTests
+    {
+        // Verify generated container names against the Azure blob container naming rules
+        // from https://msdn.microsoft.com/en-us/library/azure/dd135715.aspx
+
+        [Property]
+        public void ContainerNamesMustStartWithALetterOrNumber(BatchId jobId)
+        {
+            var containerName = ContainerNameUtils.GetSafeContainerName(jobId.ToString());
+            Assert.True(Char.IsLetterOrDigit(containerName[0]));
+        }
+
+        [Property]
+        public void ContainerNamesCanContainOnlyLettersNumbersAndDashes(BatchId jobId)
+        {
+            var containerName = ContainerNameUtils.GetSafeContainerName(jobId.ToString());
+            Assert.All(containerName,
+                ch => Assert.True(Char.IsLetterOrDigit(ch) || ch == '-'));
+        }
+
+        private static IEnumerable<int> IndexesOf(string str, char ch)
+        {
+            return Enumerable.Range(0, str.Length)
+                             .Where(i => str[i] == ch);
+        }
+
+        [Property]
+        public void EveryDashInAContainerNameMustBeImmediatelyPrecededAndFollowedByALetterOrNumber(BatchId jobId)
+        {
+            var containerName = ContainerNameUtils.GetSafeContainerName(jobId.ToString());
+
+            var dashIndexesInContainerName = IndexesOf(containerName, '-');
+
+            Assert.All(dashIndexesInContainerName,
+                i => Assert.True(Char.IsLetterOrDigit(containerName[i - 1]) && Char.IsLetterOrDigit(containerName[i + 1])));
+        }
+
+        [Property]
+        public void ConsecutiveDashesAreNotPermittedInContainerNames(BatchId jobId)
+        {
+            var containerName = ContainerNameUtils.GetSafeContainerName(jobId.ToString());
+            Assert.DoesNotContain("--", containerName);
+        }
+
+        [Property]
+        public void AllLettersInAContainerNameMustBeLowercase(BatchId jobId)
+        {
+            var containerName = ContainerNameUtils.GetSafeContainerName(jobId.ToString());
+            Assert.All(containerName,
+                ch => Assert.True(!Char.IsLetter(ch) || Char.IsLower(ch)));
+        }
+
+        [Property]
+        public void ContainerNamesMustBeFrom3Through63CharactersLong(BatchId jobId)
+        {
+            var containerName = ContainerNameUtils.GetSafeContainerName(jobId.ToString());
+            Assert.InRange(containerName.Length, 3, 63);
+        }
+
+        [Property]
+        public void ValidContainerNamesAreNotMunged(BatchIdThatIsValidContainerName jobId)
+        {
+            var containerName = ContainerNameUtils.GetSafeContainerName(jobId.ToString());
+            Assert.Equal(jobId.ToString(), containerName);
+        }
+    }
+}
---
+++ AzureBatchFileConventions/Utilities/ContainerNameUtils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Batch.Conventions.Files.Utilities
+{
+    internal static class ContainerNameUtils
+    {
+        private static readonly Regex PermittedContainerNameChars = new Regex("^[-a-z0-9]{3,63}$");
+        private static readonly HashAlgorithm hasher = new SHA1CryptoServiceProvider();
+        private static readonly int MaxJobIdLengthInMungedContainerName = 15;  // must be <= 63 - 1 - length of hash string (40 for SHA1)
+        private static readonly char[] ForbiddenLeadingTrailingContainerNameChars = new[] { '-' };
+        private static readonly Regex UnderscoresAndMultipleDashes = new Regex("[_-]+");
+
+        internal static string GetSafeContainerName(string jobId)
+        {
+            jobId = jobId.ToLowerInvariant();  // it's safe to do this early as job ids cannot differ only by case, so the lower case job id is still a unique identifier
+
+            if (!PermittedContainerNameChars.IsMatch(jobId))
+            {
+                return MungeToContainerName(jobId);
+            }
+
+            if (!Char.IsLetterOrDigit(jobId[0]) || !Char.IsLetterOrDigit(jobId[jobId.Length - 1]) || jobId.Contains("--"))
+            {
+                return MungeToContainerName(jobId);
+            }
+
+            return jobId;
+        }
+
+        private static string MungeToContainerName(string str)
+        {
+            var hash = hasher.ComputeHash(Encoding.UTF8.GetBytes(str));
+            var hashText = ToHex(hash);
+
+            var safeStr = MakeDashSafe(str);
+
+            safeStr = safeStr.Trim(ForbiddenLeadingTrailingContainerNameChars);
+
+            if (safeStr.Length > MaxJobIdLengthInMungedContainerName)
+            {
+                safeStr = safeStr.Substring(0, MaxJobIdLengthInMungedContainerName)
+                                 .Trim(ForbiddenLeadingTrailingContainerNameChars);  // do this again as truncation may have unleashed a trailing dash
+            }
+            else if (safeStr.Length == 0)
+            {
+                safeStr = "job";
+            }
+
+            return safeStr + "-" + hashText;
+        }
+
+        private static string MakeDashSafe(string rawString)
+        {
+            return UnderscoresAndMultipleDashes.Replace(rawString, "-");
+        }
+
+        private static string ToHex(byte[] bytes)
+        {
+            var sb = new StringBuilder(bytes.Length * 2);
+            foreach (var b in bytes)
+            {
+                sb.AppendFormat(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
